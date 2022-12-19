c305e4da191a9dcc71d724970eb3262f37865b8b
0nik0nik0 <119619487+0nik0nik0@users.noreply.github.com>
2022-12-19 17:04:57 +0300

    2ndExample

---
+++ lection1/example002_UserName/Program.cs
@@ -0,0 +1,11 @@
+﻿/*
+Console.WriteLine("Type your name");
+string username = Console.ReadLine();
+Console.WriteLine("HI, ");
+Console.WriteLine(username);
+*/
+
+Console.Write("Type your name ");
+string username = Console.ReadLine();
+Console.Write("HI, ");
+Console.Write(username);

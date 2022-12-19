ba742a9eb1c69876937dc5c9a594f1286af44c0a
0nik0nik0 <119619487+0nik0nik0@users.noreply.github.com>
2022-12-19 17:38:32 +0300

    names

---
+++ lection1/example5_HiUser/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Type Your name:  ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "nikita")
+{
+    Console.WriteLine("Hooray, it's Nikita!");
+}
+else
+{
+    Console.Write ("HI,  ");
+    Console.WriteLine(username);
+}

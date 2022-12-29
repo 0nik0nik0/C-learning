2c80738c3f1c0c775a5569bfba9643989b5b8250
0nik0nik0 <119619487+0nik0nik0@users.noreply.github.com>
2022-12-30 01:07:21 +0300

    Additional Practic branch1.1

--- HW3_C#/Работа 1.1. Алгоритмическая конструкция “ветвление”/5/Program.cs
+++ HW3_C#/Работа 1.1. Алгоритмическая конструкция “ветвление”/5/Program.cs
@@ -2,5 +2,18 @@
 из сообщений "Норма", "Нужно похудеть", "Нужно поправиться".
 Нормальный вес (в кг) = (рост (в см) – 100) ± 10%. (оператор if ) */
 
+Console.WriteLine("Enter your weight(kg): ");
+int weight = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter your height(cm): ");
+int height = int.Parse(Console.ReadLine());
 
 
+
+if (weight > (height - 100) * 1.1)
+ Console.WriteLine("Sorry, you're FAT");
+
+else if (weight < (height - 100) * 0.9 )
+ Console.WriteLine("Go eat smth, you are close to disappear!");
+
+else 
+ {Console.WriteLine("Congratulations! Your are perfect!");}

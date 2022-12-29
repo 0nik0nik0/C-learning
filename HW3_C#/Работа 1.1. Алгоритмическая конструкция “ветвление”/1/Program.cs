1d6520e26918771df802e10b547bf3eeefee730f
0nik0nik0 <119619487+0nik0nik0@users.noreply.github.com>
2022-12-30 00:27:47 +0300

    Additional Practic branch

---
+++ HW3_C#/Работа 1.1. Алгоритмическая конструкция “ветвление”/1/Program.cs
@@ -0,0 +1,16 @@
+﻿/*  1. Генерируется случайное целое число n в интервале [ 20 – 99 ].
+Проверяется, делится ли оно на введенное с клавиатуры целое число d
+(используйте тернарный оператор).
+ */
+
+ Random rnd = new Random(); // создаем экземпляр класса Random
+// генерируем случайное целое число в интервале 20, 99
+int n = rnd.Next(20, 100);
+Console.WriteLine($"Generated random number (n): {n}"); 
+Console.WriteLine("Type integer numder (b) ");
+// ввод строки и преобразование в целое число
+int b = int.Parse(Console.ReadLine());
+// используем тернарный оператор
+string mes1 = (n % b == 0) ?  "n divided into b" : "n not divisible by b";
+Console.WriteLine(mes1);
+Console.ReadKey();
---
+++ HW3_C#/Работа 1.1. Алгоритмическая конструкция “ветвление”/4/Program.cs
@@ -0,0 +1,31 @@
+﻿/* 4. По введенному номеру месяца выводится название поры года (зима, весна,
+лето, осень) и сообщение: сессия, каникулы, 1 семестр, 2 семестр ( if ).
+ */
+
+/* string[] array = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" }; */
+Console.WriteLine($"Type month number: ");
+int month = int.Parse(Console.ReadLine());
+
+if (month < 3 && month > 0 || month == 12) 
+    {Console.WriteLine("Winter");
+    if (month == 1) Console.WriteLine("Session but then Holidays!");
+    else if (month == 2) Console.WriteLine("2nd term");
+    else Console.WriteLine("1rd term");}
+   
+else if (month > 2 && month < 6)
+    {Console.WriteLine("Spring");
+    if (month < 5) Console.WriteLine("2nd term");
+    else Console.WriteLine("Session but then Holidays!");}
+
+else if (month > 5 && month < 9)
+    {Console.WriteLine("Summer");
+    if (month < 6) Console.WriteLine("Session;( ");
+    else Console.WriteLine("Just relax, its Summer time!");}
+    
+
+else if (month > 8 && month < 12) 
+    Console.WriteLine("Autumn \n1nd term");
+     
+
+else {Console.WriteLine($"Hey dude, wake up!");}
+
